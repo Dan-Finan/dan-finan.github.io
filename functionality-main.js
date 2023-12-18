4c874709eeffc65eb758bb51216e737eb335eda6
Dan Finan <67194093+Dan-Finan@users.noreply.github.com>
2023-12-18 14:25:30 +0000

    Local Project Upload

---
+++ functionality-main.js
@@ -0,0 +1,18 @@
+// Page Load
+window.onload = function() {
+    document.getElementById("content-main-about-section").hidden = true
+};
+
+// Other JavaScript code
+
+// Home Logo Button
+ function show_home() {
+    document.getElementById('content-main-home').hidden = false
+    document.getElementById("content-main-about-section").hidden = true
+ }
+
+// About Button
+function show_about() {
+    document.getElementById('content-main-home').hidden = true
+    document.getElementById("content-main-about-section").hidden = false
+ }
